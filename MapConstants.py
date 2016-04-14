783ea0bf08e094ad39974a6196f3f4ff2555dcde
Thomas Locher <thomas.locher@ch.abb.com>
2016-04-14 10:12:25 +0200

    Added MapConstants.py, which contains all constants used in the project.

---
+++ MapConstants.py
@@ -0,0 +1,17 @@
+"""
+MapConstants defines constants used by map.py.
+
+Information about map is available at https://github.com/THLO/map.
+"""
+
+placeholder = '_'
+placeholderFileName = '-'
+placeholderPath = '&'
+placeholderExtension = '#'
+placeholderCounter = '%'
+
+# Since any single '%' character is interpreted as the start of an
+# argument specifier, we need '%%' for the help text:
+
+placeholderCounterHelpVersion = '%%'
+escape_char = '\\'
