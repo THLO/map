e66d1b7756a2d9d450b4c6a25b0e682ed2d4f804
Thomas Locher <thomas.locher@ch.abb.com>
2016-05-11 15:55:57 +0200

    Cleaned up the method format_help().

--- MapArgumentParser.py
+++ MapArgumentParser.py
@@ -32,11 +32,11 @@ Each file is moved to its respective parent directory.",formatter_class=argparse
 	version = info['__version__']
         versionText = info['__version_text__']
         # Add all arguments:
-	# Get a group for mutually exclusive options:
-	group = self.add_mutually_exclusive_group()
+	# Get a group for the mutually exclusive options '-x' and '-d':
+	groupXD = self.add_mutually_exclusive_group()
         self.add_argument("-c", "--count-from", type=checkNegative,default=0,help="set the internal counter to the provided start value.")
         # It is not allowed to use the extension option (-x) with the directories option (-d)
-        group.add_argument("-d", "--directories", action="store_true",help="apply the command to directories instead of files.")
+        groupXD.add_argument("-d", "--directories", action="store_true",help="apply the command to directories instead of files.")
         self.add_argument("-i", "--ignore-errors", action="store_true", help="continue to execute commands even when a command has failed.")
         self.add_argument("-l", "--list", action="store_true", help="list all commands without executing them.")
         self.add_argument("-n", "--number-length", type=checkNegative,default=0,help="format the counter that is used with \
@@ -44,15 +44,18 @@ Each file is moved to its respective parent directory.",formatter_class=argparse
         self.add_argument("-r", "--recursive", action="store_true",help="search for files recursively under the provided path.")
         self.add_argument("-v", "--verbose", action="store_true", help="display detailed information about the process.")
         self.add_argument("-V",'--version', action='version', version='map '+version+'\n'+versionText,help="display information about the installed version.")
-        group.add_argument("-x", "--extensions", help="apply the command to all files with any of the listed extensions.\
+        groupXD.add_argument("-x", "--extensions", help="apply the command to all files with any of the listed extensions.\
  The extensions must be provided in a comma-separated list. By default, the command is \
 applied to all files under the provided path.")
         self.add_argument("command", help="The command that is applied to all matching files/directories.")
         self.add_argument("path",nargs='*', help="The (top-level) path where matching files are sought.")
 
     def format_help(self):
-        """ The help statement is slightly changed in that the '.py' extension is dropped. """
-	return super(MapArgumentParser,self).format_help().replace('%%','%').replace('COUNT_FROM','VALUE').replace('NUMBER_LENGTH','LENGTH').replace('EXTENSIONS','EXT')
+        """ The help statement is slightly changed in that:
+        1) MapConstants.placeholderCounterHelpVersion is replaced MapConstants.placeholderCounter
+        2) 'COUNT_FROM', 'NUMBER_LENGTH', and 'EXTENSIONS' are shortened to 'VALUE', 'LENGTH', and 'EXT', respectively.
+        """
+	return super(MapArgumentParser,self).format_help().replace(MapConstants.placeholderCounterHelpVersion,MapConstants.placeholderCounter).replace('COUNT_FROM','VALUE').replace('NUMBER_LENGTH','LENGTH').replace('EXTENSIONS','EXT')
 
 def checkNegative(value):
     errorMessage = "%s is invalid because only non-negative integers are allowed." % value
