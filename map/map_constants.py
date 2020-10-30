29ec274ab234d0d56ac14336efbf91914279a4e5
Thomas Locher <thomas.locher@ch.abb.com>
2020-10-30 11:31:19 +0100

    The names of constants are now in upper case.

--- map/map_constants.py
+++ map/map_constants.py
@@ -1,27 +1,27 @@
 """
-MapConstants defines constants used by map.
+map_constants defines constants used by map.
 
 Information about map is available at https://github.com/THLO/map.
 """
 
 # These are the main placeholders used by map:
 
-placeholder = '_'
-placeholderFileName = ':'
-placeholderPath = '&'
-placeholderExtension = '#'
-placeholderCounter = '%'
+PLACEHOLDER = '_'
+PLACEHOLDER_FILENAME = ':'
+PLACEHOLDER_PATH = '&'
+PLACEHOLDER_EXTENSION = '#'
+PLACEHOLDER_COUNTER = '%'
 
 # Since any single '%' character is interpreted as the start of an
 # argument specifier, we need '%%' for the help text:
 
-placeholderCounterHelpVersion = '%%'
+PLACEHOLDER_COUNTER_HELP_TEXT = '%%'
 
 # The standard escape character is used:
 
-escape_char = '\\'
+ESCAPE_CHARACTER = '\\'
 
 # The following placeholder is used to filter for files without
 # extensions:
 
-placeholderNoExtensionFilter = '^'
+PLACEHOLDER_NO_EXTENSION_FILTER = '^'
